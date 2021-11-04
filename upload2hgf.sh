42031f117c5f94c581e66a88c33a32b227f6b65d
t-k- <clock126@126.com>
2021-11-04 19:00:45 -0400

    added upload2hgf.sh

---
+++ upload2hgf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+git lfs install
+find ckpt -type f | xargs -I{} cp {} .
+git lfs track events.out.*
+git lfs track pytorch_model.bin
+tmp_branch=hgf-`uuidgen`
+git co -b tmp_branch
+git add .
+git commit -am 'update model'
+git push -f hgf tmp_branch:main
