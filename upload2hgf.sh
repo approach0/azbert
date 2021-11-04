5bbd6ffe56e30684ccd47d1a3d11ad02464c01fe
t-k- <clock126@126.com>
2021-11-04 19:11:13 -0400

    fix upload2hgf.sh

--- upload2hgf.sh
+++ upload2hgf.sh
@@ -5,8 +5,9 @@ find ckpt -type f | xargs -I{} cp {} .
 git lfs track events.out.*
 git lfs track pytorch_model.bin
 tmp_branch=hgf-`uuidgen`
-git checkout -b tmp_branch
+git checkout -b $tmp_branch
 git add .
 git commit -am 'update model'
-git push -f hgf tmp_branch:main
+git push -f hgf $tmp_branch:main
 git checkout -
+git branch -D $tmp_branch
